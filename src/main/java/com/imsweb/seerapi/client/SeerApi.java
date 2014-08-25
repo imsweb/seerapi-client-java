5d0e6453ecdaa1ac38d6a067b378a17f731db624
Chuck May <mayc@imsweb.com>
2014-08-25 16:22:57 -0400

    Added API methods for SEER*Rx.
    Unit tests are commented out until the Rx API goes live.

--- src/main/java/com/imsweb/seerapi/client/SeerApi.java
+++ src/main/java/com/imsweb/seerapi/client/SeerApi.java
@@ -51,6 +51,11 @@ import com.imsweb.seerapi.client.glossary.GlossaryVersion;
 import com.imsweb.seerapi.client.naaccr.NaaccrField;
 import com.imsweb.seerapi.client.naaccr.NaaccrFieldName;
 import com.imsweb.seerapi.client.naaccr.NaaccrVersion;
+import com.imsweb.seerapi.client.rx.Rx;
+import com.imsweb.seerapi.client.rx.RxChangelog;
+import com.imsweb.seerapi.client.rx.RxSearch;
+import com.imsweb.seerapi.client.rx.RxSearchResults;
+import com.imsweb.seerapi.client.rx.RxVersion;
 import com.imsweb.seerapi.client.shared.Version;
 import com.imsweb.seerapi.client.siterecode.SiteRecode;
 
@@ -506,4 +511,60 @@ public final class SeerApi {
 
         return getBuilder(target).get(new GenericType<List<GlossaryChangelog>>() {});
     }
+
+    /**
+     * Return a list of all Rx versions and information about them
+     * @return a list of RxVersion objects
+     */
+    public List<RxVersion> rxVersions() {
+        WebTarget target = createTarget("/rx/versions");
+
+        return getBuilder(target).get(new GenericType<List<RxVersion>>() {});
+    }
+
+    /**
+     * Return a complete Rx entity based in identifier
+     * @param version Rx version
+     * @param id Rx identifier
+     * @return a Rx object
+     */
+    public Rx rxById(String version, String id) {
+        WebTarget target = createTarget("/rx/{version}/id/{id}").resolveTemplate("version", version).resolveTemplate("id", id);
+
+        return getBuilder(target).get(Rx.class);
+    }
+
+    /**
+     * Return a list of matching Rx entities
+     * @param version Rx version
+     * @param search RxSearch object
+     * @return a RxSearchResults object
+     */
+    public RxSearchResults rxSearch(String version, RxSearch search) {
+        WebTarget target = createTarget("/rx/{version}").resolveTemplate("version", version);
+
+        target = target.queryParam("q", search.getQuery()).queryParam("type", search.getType()).queryParam("do_not_code", search.getDoNotCode())
+                .queryParam("category", search.getCategory()).queryParam("mode", search.getMode()).queryParam("status", search.getStatus())
+                .queryParam("assigned_to", search.getAssignedTo()).queryParam("modified_from", search.getModifiedFrom()).queryParam("modified_to", search.getModifiedTo())
+                .queryParam("published_from", search.getPublishedFrom()).queryParam("published_to", search.getPublishedTo())
+                .queryParam("been_published", search.getBeenPublished()).queryParam("hidden", search.getHidden()).queryParam("count", search.getCount())
+                .queryParam("count_only", search.getCountOnly()).queryParam("glossary", search.getIncludeGlossary()).queryParam("output_type", search.getOutputType());
+
+        return getBuilder(target).get(RxSearchResults.class);
+    }
+
+    /**
+     * Return the changelog entries for the passed database version
+     * @param version Rx version
+     * @param fromDate if not null, only include changes from this date forward (YYYY-MM-DD)
+     * @param toDate if not null, only include changes prior to this date (YYYY-MM-DD)
+     * @param count if not null, limit the number returned
+     * @return a list of RxChangelog objects
+     */
+    public List<RxChangelog> rxChangelogs(String version, String fromDate, String toDate, Integer count) {
+        WebTarget target = createTarget("/rx/{version}/changelog").resolveTemplate("version", version).queryParam("from", fromDate).queryParam("to", toDate).queryParam("count", count);
+
+        return getBuilder(target).get(new GenericType<List<RxChangelog>>() {});
+    }
+
 }

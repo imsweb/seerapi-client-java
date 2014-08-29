01cf0d406522f3e2309dd7f7f2f1bd90a4360d69
Chuck May <mayc@imsweb.com>
2014-08-29 16:32:12 -0400

    Automating SNAPSHOT deployments to Sonotype OSS

---
+++ config/publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script initiates the Gradle publishing task when pushes to master occur.  This was borrowed
+# from https://github.com/ReadyTalk/swt-bling/blob/master/.utility/initiate-publish.sh.
+#
+# NOTE: Travis-CI can only publish SNAPSHOT versions.
+
+if [ "$TRAVIS_REPO_SLUG" == "imsweb/seerapi-client-java" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
+      echo 'Travis can only publish snapshots.'
+      return 0
+  fi
+
+  echo -e "Starting publish to Sonatype...\n"
+
+  ./gradlew uploadArchives -PnexusUsername="${NEXUS_USERNAME}" -PnexusPassword="${NEXUS_PASSWORD}"
+  RETVAL=$?
+
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed publish!'
+  else
+    echo 'Publish failed.'
+    return 1
+  fi
+
+fi

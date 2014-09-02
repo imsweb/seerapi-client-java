39fd8ec25e4a7919a68d88862efa70ac5acb0b82
Chuck May <mayc@imsweb.com>
2014-09-02 09:41:26 -0400

    Switch to bintray for publishing

--- config/publish.sh
+++ config/publish.sh
@@ -2,17 +2,17 @@
 # This script initiates the Gradle publishing task when pushes to master occur.  This was borrowed
 # from https://github.com/ReadyTalk/swt-bling/blob/master/.utility/initiate-publish.sh.
 #
-# NOTE: Travis-CI can only publish SNAPSHOT versions.
+# NOTE: Snapshot versions cannot be uploaded to bintray
 
 if [ "$TRAVIS_REPO_SLUG" == "imsweb/seerapi-client-java" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  if [[ $(./gradlew -q getVersion) != *SNAPSHOT* ]]; then
-      echo 'Travis can only publish snapshots.'
+  if [[ $(./gradlew -q getVersion) == *SNAPSHOT* ]]; then
+      echo 'Bintray cannot publish snapshots.'
       exit 0
   fi
 
   echo -e "Starting publish to Sonatype...\n"
 
-  ./gradlew uploadArchives -PnexusUsername="${NEXUS_USERNAME}" -PnexusPassword="${NEXUS_PASSWORD}"
+  ./gradlew uploadBintray -PbintrayUser="${BINTRAY_USER}" -PbintrayKey="${BINTRAY_KEY}"
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then

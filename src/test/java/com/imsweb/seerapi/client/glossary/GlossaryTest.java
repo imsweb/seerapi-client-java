364d108d5f5e237ed0a0ea0f771eefbb2122581c
Chuck May <mayc@imsweb.com>
2014-09-22 14:45:20 -0400

    Fixed glossary unit test

--- src/test/java/com/imsweb/seerapi/client/glossary/GlossaryTest.java
+++ src/test/java/com/imsweb/seerapi/client/glossary/GlossaryTest.java
@@ -34,7 +34,10 @@ public class GlossaryTest {
 
     @Test
     public void testGlossaryById() throws IOException {
-        Glossary glossary = SeerApi.connect().glossaryById("latest", "51f6cf85e3e2d4d91c7a7296");
+        GlossarySearchResults results = SeerApi.connect().glossarySearch("latest", new GlossarySearch("stem"));
+        Assert.assertTrue(results.getCount() > 0);
+
+        Glossary glossary = SeerApi.connect().glossaryById("latest", results.getResults().get(0).getId());
 
         Assert.assertNotNull(glossary);
         Assert.assertEquals("Brain stem", glossary.getName());

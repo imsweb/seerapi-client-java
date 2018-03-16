998b68fca33ee899da42703abff7289db91b9dc2
Chuck May <mayc@imsweb.com>
2018-03-16 09:29:42 -0400

    Test cleanup.

--- src/test/java/com/imsweb/seerapi/client/staging/StagingTest.java
+++ src/test/java/com/imsweb/seerapi/client/staging/StagingTest.java
@@ -12,8 +12,7 @@ import org.junit.Test;
 import com.imsweb.seerapi.client.SeerApi;
 import com.imsweb.seerapi.client.staging.cs.CsSchemaLookup;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StagingTest {
 
@@ -31,90 +30,89 @@ public class StagingTest {
     public void testGetAlgorithms() throws IOException {
         List<StagingAlgorithm> algorithms = _STAGING.algorithms().execute().body();
 
-        assertTrue(algorithms.size() > 0);
+        assertThat(algorithms).isNotEmpty().extracting("algorithm").contains("cs", "tnm", "eod_public");
     }
 
     @Test
     public void testGetAlgorithmVersions() throws IOException {
         List<StagingVersion> versions = _STAGING.versions(_ALGORITHM).execute().body();
 
-        assertTrue(versions.size() > 0);
+        assertThat(versions).isNotEmpty();
     }
 
     @Test
     public void testListSchemas() throws IOException {
         List<StagingSchemaInfo> schemaInfos = _STAGING.schemas(_ALGORITHM, _VERSION).execute().body();
-        assertTrue(schemaInfos.size() > 0);
+        assertThat(schemaInfos).isNotEmpty();
 
         schemaInfos = _STAGING.schemas(_ALGORITHM, _VERSION, "skin").execute().body();
-        assertTrue(schemaInfos.size() > 0);
+        assertThat(schemaInfos).isNotEmpty();
     }
 
     @Test
     public void testSchemaLookup() throws IOException {
         // first test simple lookup that returns a single schema with site/hist only
         List<StagingSchemaInfo> schemas = _STAGING.schemaLookup(_ALGORITHM, _VERSION, new CsSchemaLookup("C509", "8000").getInputs()).execute().body();
-        assertEquals(1, schemas.size());
-        assertEquals("breast", schemas.get(0).getId());
+        assertThat(schemas).hasSize(1).extracting("id").contains("breast");
 
         // now test just site
         SchemaLookup data = new SchemaLookup();
         data.setInput(StagingData.PRIMARY_SITE_KEY, "C111");
-        assertEquals(7, _STAGING.schemaLookup(_ALGORITHM, _VERSION, data.getInputs()).execute().body().size());
+        assertThat(_STAGING.schemaLookup(_ALGORITHM, _VERSION, data.getInputs()).execute().body()).hasSize(7);
 
         // add histology
         data.setInput(StagingData.HISTOLOGY_KEY, "8000");
-        assertEquals(2, _STAGING.schemaLookup(_ALGORITHM, _VERSION, data.getInputs()).execute().body().size());
+        assertThat(_STAGING.schemaLookup(_ALGORITHM, _VERSION, data.getInputs()).execute().body()).hasSize(2);
 
         // add discriminator
         data.setInput("ssf25", "010");
         schemas = _STAGING.schemaLookup(_ALGORITHM, _VERSION, data.getInputs()).execute().body();
-        assertEquals(1, schemas.size());
-        assertEquals("nasopharynx", schemas.get(0).getId());
+        assertThat(schemas).hasSize(1).extracting("id").contains("nasopharynx");
 
         // test with the CsStaging class
         schemas = _STAGING.schemaLookup(_ALGORITHM, _VERSION, new CsSchemaLookup("C111", "8000", "010").getInputs()).execute().body();
-        assertEquals(1, schemas.size());
-        assertEquals("nasopharynx", schemas.get(0).getId());
+        assertThat(schemas).hasSize(1).extracting("id").contains("nasopharynx");
     }
 
     @Test
     public void testSchemaById() throws IOException {
         StagingSchema schema = _STAGING.schemaById(_ALGORITHM, _VERSION, "brain").execute().body();
 
-        assertEquals("cs", schema.getAlgorithm());
-        assertEquals("02.05.50", schema.getVersion());
-        assertEquals("brain", schema.getId());
+        assertThat(schema).isNotNull();
+        assertThat(schema.getAlgorithm()).isEqualTo("cs");
+        assertThat(schema.getVersion()).isEqualTo("02.05.50");
+        assertThat(schema.getId()).isEqualTo("brain");
     }
 
     @Test
     public void testSchemaInvolvedTables() throws IOException {
         List<StagingTable> tables = _STAGING.involvedTables(_ALGORITHM, _VERSION, "brain").execute().body();
 
-        assertTrue(tables.size() > 0);
+        assertThat(tables).isNotEmpty();
     }
 
     @Test
     public void testListTables() throws IOException {
         List<StagingTable> tables = _STAGING.tables(_ALGORITHM, _VERSION, "ssf1").execute().body();
 
-        assertTrue(tables.size() > 0);
+        assertThat(tables).isNotEmpty();
     }
 
     @Test
     public void testTableById() throws IOException {
         StagingTable table = _STAGING.tableById(_ALGORITHM, _VERSION, "primary_site").execute().body();
 
-        assertEquals("cs", table.getAlgorithm());
-        assertEquals("02.05.50", table.getVersion());
-        assertEquals("primary_site", table.getId());
+        assertThat(table).isNotNull();
+        assertThat(table.getAlgorithm()).isEqualTo("cs");
+        assertThat(table.getVersion()).isEqualTo("02.05.50");
+        assertThat(table.getId()).isEqualTo("primary_site");
     }
 
     @Test
     public void testTableInvoledSchemas() throws IOException {
         List<StagingSchema> schemas = _STAGING.involvedSchemas(_ALGORITHM, _VERSION, "extension_baa").execute().body();
 
-        assertTrue(schemas.size() > 0);
+        assertThat(schemas).isNotEmpty();
     }
 
     @Test
@@ -143,58 +141,59 @@ public class StagingTest {
         // perform the staging
         StagingData output = _STAGING.stage(_ALGORITHM, _VERSION, data.getInput()).execute().body();
 
-        assertEquals(StagingData.Result.STAGED, output.getResult());
-        assertEquals(0, output.getErrors().size());
-        assertEquals(37, output.getPath().size());
+        assertThat(output).isNotNull();
+        assertThat(output.getResult()).isEqualTo(StagingData.Result.STAGED);
+        assertThat(output.getErrors()).isEmpty();
+        assertThat(output.getPath()).hasSize(37);
 
         // check output
-        assertEquals("129", output.getOutput("schema_number"));
-        assertEquals("urethra", output.getSchemaId());
-        assertEquals("020550", output.getOutput("csver_derived"));
+        assertThat(output.getOutput("schema_number")).isEqualTo("129");
+        assertThat(output.getSchemaId()).isEqualTo("urethra");
+        assertThat(output.getOutput("csver_derived")).isEqualTo("020550");
 
         // AJCC 6
-        assertEquals("T1", output.getOutput("ajcc6_t"));
-        assertEquals("10", output.getOutput("stor_ajcc6_t"));
-        assertEquals("c", output.getOutput("ajcc6_tdescriptor"));
-        assertEquals("c", output.getOutput("stor_ajcc6_tdescriptor"));
-        assertEquals("N1", output.getOutput("ajcc6_n"));
-        assertEquals("10", output.getOutput("stor_ajcc6_n"));
-        assertEquals("c", output.getOutput("ajcc6_ndescriptor"));
-        assertEquals("c", output.getOutput("stor_ajcc6_ndescriptor"));
-        assertEquals("M1", output.getOutput("ajcc6_m"));
-        assertEquals("10", output.getOutput("stor_ajcc6_m"));
-        assertEquals("c", output.getOutput("ajcc6_mdescriptor"));
-        assertEquals("c", output.getOutput("stor_ajcc6_mdescriptor"));
-        assertEquals("IV", output.getOutput("ajcc6_stage"));
-        assertEquals("70", output.getOutput("stor_ajcc6_stage"));
+        assertThat(output.getOutput("ajcc6_t")).isEqualTo("T1");
+        assertThat(output.getOutput("stor_ajcc6_t")).isEqualTo("10");
+        assertThat(output.getOutput("ajcc6_tdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("stor_ajcc6_tdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("ajcc6_n")).isEqualTo("N1");
+        assertThat(output.getOutput("stor_ajcc6_n")).isEqualTo("10");
+        assertThat(output.getOutput("ajcc6_ndescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("stor_ajcc6_ndescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("ajcc6_m")).isEqualTo("M1");
+        assertThat(output.getOutput("stor_ajcc6_m")).isEqualTo("10");
+        assertThat(output.getOutput("ajcc6_mdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("stor_ajcc6_mdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("ajcc6_stage")).isEqualTo("IV");
+        assertThat(output.getOutput("stor_ajcc6_stage")).isEqualTo("70");
 
         // AJCC 7
-        assertEquals("T1", output.getOutput("ajcc7_t"));
-        assertEquals("100", output.getOutput("stor_ajcc7_t"));
-        assertEquals("c", output.getOutput("ajcc7_tdescriptor"));
-        assertEquals("c", output.getOutput("stor_ajcc7_tdescriptor"));
-        assertEquals("N1", output.getOutput("ajcc7_n"));
-        assertEquals("100", output.getOutput("stor_ajcc7_n"));
-        assertEquals("c", output.getOutput("ajcc7_ndescriptor"));
-        assertEquals("c", output.getOutput("stor_ajcc7_ndescriptor"));
-        assertEquals("M1", output.getOutput("ajcc7_m"));
-        assertEquals("100", output.getOutput("stor_ajcc7_m"));
-        assertEquals("c", output.getOutput("ajcc7_mdescriptor"));
-        assertEquals("c", output.getOutput("stor_ajcc7_mdescriptor"));
-        assertEquals("IV", output.getOutput("ajcc7_stage"));
-        assertEquals("700", output.getOutput("stor_ajcc7_stage"));
+        assertThat(output.getOutput("ajcc7_t")).isEqualTo("T1");
+        assertThat(output.getOutput("stor_ajcc7_t")).isEqualTo("100");
+        assertThat(output.getOutput("ajcc7_tdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("stor_ajcc7_tdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("ajcc7_n")).isEqualTo("N1");
+        assertThat(output.getOutput("stor_ajcc7_n")).isEqualTo("100");
+        assertThat(output.getOutput("ajcc7_ndescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("stor_ajcc7_ndescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("ajcc7_m")).isEqualTo("M1");
+        assertThat(output.getOutput("stor_ajcc7_m")).isEqualTo("100");
+        assertThat(output.getOutput("ajcc7_mdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("stor_ajcc7_mdescriptor")).isEqualTo("c");
+        assertThat(output.getOutput("ajcc7_stage")).isEqualTo("IV");
+        assertThat(output.getOutput("stor_ajcc7_stage")).isEqualTo("700");
 
         // Summary Stage
-        assertEquals("L", output.getOutput("t77"));
-        assertEquals("RN", output.getOutput("n77"));
-        assertEquals("D", output.getOutput("m77"));
-        assertEquals("D", output.getOutput("ss77"));
-        assertEquals("7", output.getOutput("stor_ss77"));
-        assertEquals("L", output.getOutput("t2000"));
-        assertEquals("RN", output.getOutput("n2000"));
-        assertEquals("D", output.getOutput("m2000"));
-        assertEquals("D", output.getOutput("ss2000"));
-        assertEquals("7", output.getOutput("stor_ss2000"));
+        assertThat(output.getOutput("t77")).isEqualTo("L");
+        assertThat(output.getOutput("n77")).isEqualTo("RN");
+        assertThat(output.getOutput("m77")).isEqualTo("D");
+        assertThat(output.getOutput("ss77")).isEqualTo("D");
+        assertThat(output.getOutput("stor_ss77")).isEqualTo("7");
+        assertThat(output.getOutput("t2000")).isEqualTo("L");
+        assertThat(output.getOutput("n2000")).isEqualTo("RN");
+        assertThat(output.getOutput("m2000")).isEqualTo("D");
+        assertThat(output.getOutput("ss2000")).isEqualTo("D");
+        assertThat(output.getOutput("stor_ss2000")).isEqualTo("7");
     }
 
     @Test
@@ -208,8 +207,9 @@ public class StagingTest {
         // perform the staging
         StagingData output = _STAGING.stage(_ALGORITHM, _VERSION, data.getInput()).execute().body();
 
-        assertEquals(StagingData.Result.STAGED, output.getResult());
-        assertEquals(9, output.getErrors().size());
+        assertThat(output).isNotNull();
+        assertThat(output.getResult()).isEqualTo(StagingData.Result.STAGED);
+        assertThat(output.getErrors()).hasSize(9);
     }
 
 }
